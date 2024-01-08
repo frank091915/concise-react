b168401389f966977bdb068f7ecd9cafe50186b6
Finn <Finn-FY.Fu@aia.com>
2024-01-08 13:59:17 +0800

    feat: managing state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,19 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 function ListGroup() {
   let cities = ["New York", "San Francisco", "Tokyo", "Paris"];
+  const [selectedIndex, setSelectedIndex] = useState(0)
   const message = (customMessage?: string) =>
     cities.length && <p>{customMessage && "No Item Found"}</p>;
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
+  const handleClick = (index: number) => {
+    setSelectedIndex(index)
   };
   return (
     <>
       <h1>Cites</h1>
       {message()}
       <ul className="list-group">
-        {cities.map((item) => (
-          <li key={item} onClick={handleClick} className="list-group-item">
+        {cities.map((item, index) => (
+          <li key={item} onClick={() => handleClick(index)} className={selectedIndex === index ? "list-group-item active" : 'list-group-item'}>
             {item}
           </li>
         ))}
