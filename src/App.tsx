2069010477d4bf605284500e726b517f1564da43
Finn <Finn-FY.Fu@aia.com>
2024-01-09 15:22:44 +0800

    feat: pass children

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import ListGroup from "./components/ListGroup";
 import "./App.css";
+import Alert from "./components/Alert";
 
 function App() {
   const cities = ["New York", "San Francisco", "Tokyo", "Paris"];
-  const heading = 'cities'
-  const handler = (item: string) =>{
-    console.log(item)
-  }
-  return <ListGroup items={cities} heading={heading} onSelected={handler}></ListGroup>;
+  const heading = "cities";
+  const handler = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <Alert>
+      lovely <span>tutorials</span>
+    </Alert>
+  );
 }
 
 export default App;
